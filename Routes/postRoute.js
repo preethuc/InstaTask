f8649c47910666aca2b06d7a8b771408d299478f
preethuc <preetha@sparkouttech.com>
2022-10-10 21:41:00 +0530

    updated code

--- Routes/postRoute.js
+++ Routes/postRoute.js
@@ -16,17 +16,51 @@ const fileStorageEngine = multer.diskStorage({
 });
 const upload = multer({ storage: fileStorageEngine });
 // const upload = multer({dest:'uploads/'})
-router.post("/createPost",upload.single("image"),(req, res, next) => {
-    const newPost = Post.create(req.body);
-    console.log(req.file);
+
+router.post("/createPost",upload.single("image"),async(req, res, next) => {
+
+  try{
+    const newPost = new Post({
+      user:  req.body.user,
+      postName: req.body.postName,
+      commentAndLike: req.body.commentAndLike,
+      caption: req.body.caption
+    })
+
+    if(req.file){
+      newPost.image = req.file.path
+    }
+
+    await newPost.save()
+
     // res.send('file uploaded successfully')
-    res.status(201).json({
+    return res.status(201).json({
       status: "success",
       data: {
         post: newPost,
       },
     });
+  }
+  catch (err) { 
+    console.log(err);
+    return res.send(err)
+  }
 });
+// router.post("/createPost",upload.single("image"),(req, res, next) => {
+//     const newPost = Post.create({
+//         postName: req.body.postName,
+//         user: req.body.user,
+//         caption:req.body.caption
+//     });
+//     console.log(req.file);
+//     // res.send('file uploaded successfully')
+//     res.status(201).json({
+//       status: "success",
+//       data: {
+//         post: newPost,
+//       },
+//     });
+// });
 
 
 //ROUTES
